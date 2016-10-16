274e8c4631ded742a960dd0d191088b74a6e2f61
MPilvik <maarjaliisapilvik@gmail.com>
2016-10-16 19:07:29 +0300

    Konsoolipõhine versioon

--- EestiMurded.java
+++ EestiMurded.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class EestiMurded {
+
     public static void main(String args[]) {
-        System.out.println("Eesti murdeid tutvustav projekt");
+        System.out.println("See on Eesti murdeid tutvustav projekt");
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Kas soovid kõigepealt näha murrete kaarti? (jah/ei)");
+        String vastus = input.next().toLowerCase();
+
+        if (vastus.equals("jah")) {
+            System.out.println("Varsti saab!");
+        } else if (vastus.equals("ei")) {
+            System.out.println("Hästi!");
+        }
     }
 }
