c2b4db061bb93cc9ef6d9b1076d7e6212fd690a9
Maarja-Liisa Pilvik <maarjaliisapilvik@gmail.com>
2017-01-01 00:10:03 +0200

    sõnade listi lisamine

--- EestiMurded.java
+++ EestiMurded.java
@@ -3,12 +3,16 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class EestiMurded extends Application {
@@ -16,6 +20,7 @@ public class EestiMurded extends Application {
     @Override
 
     public void start(Stage primaryStage) throws Exception {
+
         Pane pane = new Pane();
         Scene scene = new Scene(pane, 300, 400);
         primaryStage.setScene(scene);
@@ -38,26 +43,60 @@ public class EestiMurded extends Application {
         box.getChildren().add(imageview);
         root.getChildren().add(box);
 
+
+        // kasuta Sõnade klassi meetodit sõnad, et tabeli sõnade nimekirjad kätte saada
+        Sõnad words = new Sõnad();
+        ArrayList murdesõnad = (ArrayList) words.sõnad().get("murdesõnad");
+        ArrayList üldkeele_vasted = (ArrayList) words.sõnad().get("üldkeele_vasted");
+        ArrayList sõnaliik = (ArrayList) words.sõnad().get("sõnaliik");
+
+        // vali 3 suvalist erinevat rea numbrit 0st kuni tabeli pikkuseni
+        int suvaline_arv1 = (int)(Math.round(Math.random()*murdesõnad.size()));
+        int suvaline_arv2 = (int)(Math.round(Math.random()*murdesõnad.size()));
+        // kui 1. ja 2. suvaline arv on võrdsed, siis võta uus 2. suvaline arv
+        while(suvaline_arv1 == suvaline_arv2){
+            suvaline_arv2 = (int)(Math.round(Math.random()*murdesõnad.size()));
+        }
+        int suvaline_arv3 = (int)(Math.round(Math.random()*460));
+        // kui 3. suvaline arv on võrdne kas 1. või 2. suvalise arvuga, siis vali uus 3. suvaline arv
+        while(suvaline_arv3 == suvaline_arv1 || suvaline_arv3 == suvaline_arv2){
+            suvaline_arv3 = (int)(Math.round(Math.random()*460));
+        }
+
+        VBox vbox = new VBox();
+        ToggleGroup g = new ToggleGroup();
+        RadioButton e1 = new RadioButton((String) üldkeele_vasted.get(suvaline_arv1));
+        RadioButton e2 = new RadioButton((String) üldkeele_vasted.get(suvaline_arv2));
+        RadioButton e3 = new RadioButton((String) üldkeele_vasted.get(suvaline_arv3));
+        e1.setToggleGroup(g);
+        e2.setToggleGroup(g);
+        e3.setToggleGroup(g);
+        vbox.getChildren().addAll(e1, e2, e3);
+        Button button = new Button("Vasta");
+        button.setLayoutX(70);
+        button.setLayoutY(150);
+        button.setOnAction((event) -> {
+            primaryStage.setScene(scene2);
+        });
+
+        /*
         Button button = new Button("Vajuta siia!");
         button.setLayoutX(70);
         button.setLayoutY(150);
         button.setOnAction((event) -> {
-            System.out.println("Jaa");
             primaryStage.setScene(scene2);
         });
-        pane.getChildren().addAll(label, button);
+        */
+        pane.getChildren().addAll(label, button, vbox);
         primaryStage.show();
-
     }
 
     /*
     public static void main(String args[]) {
         System.out.println("See on Eesti murdeid tutvustav projekt");
-
         Scanner input = new Scanner(System.in);
         System.out.println("Kas soovid kõigepealt näha murrete kaarti? (jah/ei)");
         String vastus = input.next().toLowerCase();
-
         if (vastus.equals("jah")) {
             System.out.println("Varsti saab!");
         } else if (vastus.equals("ei")) {
---
+++ Sõnad.java
@@ -0,0 +1,58 @@
+// Impordi vajalikud paketid
+import java.io.BufferedReader;
+import java.io.FileReader; // pakett failide sisselugemiseks
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Sõnad {
+
+    public HashMap sõnad() {
+
+        // Määratle muutujate nimed
+        String csvFile = "murded.csv"; // sõnade fail csv-formaadis
+        String line = "";
+        String csvSplitBy = ";"; // eralda väljad semikooloni kohalt
+
+        // Loe faili tulbad erinevatesse listidesse
+        ArrayList<String> murdesõnad = new ArrayList<>();
+        ArrayList<String> üldkeele_vasted = new ArrayList<>();
+        ArrayList<String> sõnaliik = new ArrayList<>();
+
+        try(BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+
+            while((line = br.readLine()) != null) { // kuni rida ei ole tühi
+                String[] words = line.split(csvSplitBy); // tee tabeli ridadest sõnejärjend (eralda väljad ; koha pealt)
+                murdesõnad.add(words[0]);
+                üldkeele_vasted.add(words[1]);
+                sõnaliik.add(words[2]);
+            }
+
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        HashMap map = new HashMap();
+        map.put("murdesõnad", murdesõnad);
+        map.put("üldkeele_vasted", üldkeele_vasted);
+        map.put("sõnaliik", sõnaliik);
+
+        return map;
+        /*
+        // vali 3 suvalist erinevat rea numbrit 0st kuni tabeli pikkuseni
+        int suvaline_arv1 = (int)(Math.round(Math.random()*460));
+        int suvaline_arv2 = (int)(Math.round(Math.random()*460));
+        // kui 1. ja 2. suvaline arv on võrdsed, siis võta uus 2. suvaline arv
+        while(suvaline_arv1 == suvaline_arv2){
+            suvaline_arv2 = (int)(Math.round(Math.random()*460));
+        }
+        int suvaline_arv3 = (int)(Math.round(Math.random()*460));
+        // kui 3. suvaline arv on võrdne kas 1. või 2. suvalise arvuga, siis vali uus 3. suvaline arv
+        while(suvaline_arv3 == suvaline_arv1 || suvaline_arv3 == suvaline_arv2){
+            suvaline_arv3 = (int)(Math.round(Math.random()*460));
+        }
+        System.out.println(suvaline_arv1 + " " + suvaline_arv2 + " " + suvaline_arv3);
+        */
+    }
+
+}
